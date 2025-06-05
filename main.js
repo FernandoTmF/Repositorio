75eddc698264d163b100ae8292d3b2366be8e375
FernandoTmF <141974601+FernandoTmF@users.noreply.github.com>
2025-06-04 21:33:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-09T12:00:00");
+const tempoObjetivo1 = new Date("2025-06-09T08:00:00");
 const tempoObjetivo2 = new Date("2025-06-21T00:00:00")
 const tempoObjetivo3 = new Date("2025-08-01T00:00:00");
 const tempoObjetivo4 = new Date("2025-08-05T00:00:00");
